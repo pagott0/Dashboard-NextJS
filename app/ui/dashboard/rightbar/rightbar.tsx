efd7b55b5cb38a826a5a1d24781b77b04e10ed61
Enzo Pagotto <enzovaspagotto@gmail.com>
2024-02-20 00:33:45 -0300

    add right bar

--- app/ui/dashboard/rightbar/rightbar.tsx
+++ app/ui/dashboard/rightbar/rightbar.tsx
@@ -1,9 +1,43 @@
+import Image from 'next/image';
 import styles from './rightbar.module.css';
+import { MdPlayCircle, MdReadMore } from 'react-icons/md';
 
 const Rightbar = () => {
     return (
         <div className={styles.container}>
-            Rightbar
+            <div className={styles.item}>
+                <div className={styles.bgContainer}>
+                    <Image src="/astronaut.png" alt='' fill className={styles.bg}/>
+                </div>
+                <div className={styles.texts}>
+                    <span className={styles.notification}>Available Now</span>
+                    <h3 className={styles.title}>How to use the new version of the admin dashboard?</h3>
+                    <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+                    <p className={styles.description}>
+                        Lorem ipsun dolor sit amet consectetur adiscpigin elit.
+                        Reprehenredit eius libero persiciatis recusandae possimus.
+                    </p>
+                    <button className={styles.button}>
+                        <MdPlayCircle />
+                        Watch
+                    </button>
+                </div>
+            </div>
+            <div className={styles.item}>
+                <div className={styles.texts}>
+                    <span className={styles.notification}>Coming Soon</span>
+                    <h3 className={styles.title}>New servers actions are available, partial pre-rendering is coming up!</h3>
+                    <span className={styles.subtitle}>Boost your productivity!</span>
+                    <p className={styles.description}>
+                        Lorem ipsun dolor sit amet consectetur adiscpigin elit.
+                        Reprehenredit eius libero persiciatis recusandae possimus.
+                    </p>
+                    <button className={styles.button}>
+                        <MdReadMore />
+                        Learn
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
