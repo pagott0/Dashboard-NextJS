e9ea0dcd5b0f5dae15ac39d05f40d074817e0cbf
Enzo Pagotto <enzovaspagotto@gmail.com>
2024-02-22 15:09:34 -0300

    login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,15 @@
+import styles from "@/app/ui/login/login.module.css";
+
 const LoginPage = () => {
     return (
-        <div>LoginPage</div>
+        <div className={styles.container}>
+                <form className={styles.form}>
+                    <h1>Login</h1>
+                    <input placeholder="username" name="username" type="text"/>
+                    <input placeholder="password" name="password" type="password"/>
+                    <button>Login</button>
+                </form>
+        </div>
     )
 }
 
